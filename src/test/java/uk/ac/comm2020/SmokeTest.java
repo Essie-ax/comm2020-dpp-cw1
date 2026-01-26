da7242fc473124b534da32a9aba3bd1564ab0fe9
Essie-ax <649807813@qq.com>
2026-01-26 18:51:51 +0000

    add smoke test

---
+++ src/test/java/uk/ac/comm2020/SmokeTest.java
@@ -0,0 +1,12 @@
+package uk.ac.comm2020;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SmokeTest {
+    @Test
+    void itRuns() {
+        assertTrue(true);
+    }
+}
